9cb1395328b5d57052e175ab333cd0ea2f81a6e7
Aflah <72096386+aflah02@users.noreply.github.com>
2022-01-09 02:40:42 +0530

    WORKING

---
+++ GraphMaking/main.py
@@ -0,0 +1,11 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+G = nx.Graph()
+G.add_edge('A', 'B', weight=4)
+G.add_edge('B', 'D', weight=2)
+G.add_edge('A', 'C', weight=3)
+G.add_edge('C', 'D', weight=4)
+print(nx.shortest_path(G, 'A', 'D', weight='weight'))
+nx.draw(G)
+plt.savefig("filename.png")
+# ['A', 'B', 'D']
